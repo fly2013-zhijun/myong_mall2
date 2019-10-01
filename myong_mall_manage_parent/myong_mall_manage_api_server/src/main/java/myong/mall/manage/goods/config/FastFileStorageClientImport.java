dad77f0cdc735eda7906ee8bde2b2887f0e977e6
tom <tom@qq.com>
2019-10-01 18:50:15 +0800

    第一次提交,完成了商品分类和品牌功能

--- myong_mall_manage_parent/myong_mall_manage_api_server/src/main/java/myong/mall/manage/goods/config/FastFileStorageClientImport.java
+++ myong_mall_manage_parent/myong_mall_manage_api_server/src/main/java/myong/mall/manage/goods/config/FastFileStorageClientImport.java
@@ -21,5 +21,4 @@ import org.springframework.jmx.support.RegistrationPolicy;
 // 解决jmx重复注册bean的问题
 @EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
 public class FastFileStorageClientImport {
-
 }
